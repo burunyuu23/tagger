c4760fad37661ab58d1d5ace56a52700361b15a8
quicklybly <96150280+quicklybly@users.noreply.github.com>
2023-10-15 22:04:24 +0300

    close #11 remove beans from spring boot application (#12)

--- src/main/kotlin/ru/dnlkk/tagger/Tagger.kt
+++ src/main/kotlin/ru/dnlkk/tagger/Tagger.kt
@@ -2,12 +2,15 @@ package ru.dnlkk.tagger
 
 import api.longpoll.bots.LongPollBot
 import api.longpoll.bots.model.events.messages.MessageNew
+import jakarta.annotation.PostConstruct
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
 import ru.dnlkk.tagger.configuration.TaggerProperties
 import ru.dnlkk.tagger.infrastructure.MessageBuilder
 import ru.dnlkk.tagger.infrastructure.front_controller.TaggerFrontController
 
+@Component
 class Tagger(
     private val taggerProperties: TaggerProperties,
     private val taggerFrontController: TaggerFrontController
@@ -15,6 +18,9 @@ class Tagger(
     companion object {
         private val log: Logger = LoggerFactory.getLogger(this::class.java)
     }
+
+    @PostConstruct
+    fun start() = startPolling()
 
     override fun getAccessToken(): String {
         log.info("Get access token")
--- src/main/kotlin/ru/dnlkk/tagger/configuration/TaggerProperties.kt
+++ src/main/kotlin/ru/dnlkk/tagger/configuration/TaggerProperties.kt
@@ -1,7 +1,9 @@
 package ru.dnlkk.tagger.configuration
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 
+@Component
 class TaggerProperties {
     @Value("\${tagger.access_token}")
     val accessToken: String = ""
