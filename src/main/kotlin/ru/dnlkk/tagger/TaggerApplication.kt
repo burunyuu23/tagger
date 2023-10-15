1e6a930e2af991e53daf738c274c44e506095384
quicklybly <96150280+quicklybly@users.noreply.github.com>
2023-10-15 22:04:24 +0300

    close #11 remove beans from spring boot application (#12)

--- src/main/kotlin/ru/dnlkk/tagger/TaggerApplication.kt
+++ src/main/kotlin/ru/dnlkk/tagger/TaggerApplication.kt
@@ -2,27 +2,9 @@ package ru.dnlkk.tagger
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import ru.dnlkk.tagger.configuration.TaggerProperties
-import ru.dnlkk.tagger.infrastructure.front_controller.TaggerFrontController
 
 @SpringBootApplication
-class TaggerApplication {
-
-    @Bean
-    fun tagger(
-        properties: TaggerProperties,
-        taggerFrontController: TaggerFrontController
-    ) = Tagger(properties, taggerFrontController).apply {
-        startPolling()
-    }
-
-    @Bean
-    fun taggerProperties() = TaggerProperties()
-
-    @Bean
-    fun frontController() = TaggerFrontController()
-}
+class TaggerApplication
 
 fun main() {
     runApplication<TaggerApplication>()
