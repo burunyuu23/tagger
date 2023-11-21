f0a91ff5c70762d008d2400fcb9ed03b1a9957ac
danilkk <ezzfvkoko@gmail.com>
2023-11-21 13:15:09 +0300

    feat: add fun mapping(message: String, value: String): Boolean for default mapping + override for custom mappings rule

--- src/main/kotlin/ru/dnlkk/tagger/infrastructure/TaggerController.kt
+++ src/main/kotlin/ru/dnlkk/tagger/infrastructure/TaggerController.kt
@@ -4,4 +4,9 @@ import api.longpoll.bots.model.objects.basic.Message
 
 interface TaggerController<T> {
     fun handle(message: Message, args: Map<String, Array<String>>, messageBuilder: MessageBuilder.Builder, dto: T): MessageBuilder
+
+    fun mapping(message: String, value: String): Boolean {
+        val firstWord = message.split(" ").first()
+        return firstWord == value
+    }
 }
