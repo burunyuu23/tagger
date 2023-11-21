6d5cff6abfa207593b82203e8005e97372ffe99d
danilkk <ezzfvkoko@gmail.com>
2023-11-21 13:13:18 +0300

    refactor: make code 💫prettier💫

--- src/main/kotlin/ru/dnlkk/tagger/infrastructure/front_controller/TaggerFrontController.kt
+++ src/main/kotlin/ru/dnlkk/tagger/infrastructure/front_controller/TaggerFrontController.kt
@@ -15,16 +15,18 @@ import ru.dnlkk.tagger.infrastructure.annotation.TaggerHelp
 import ru.dnlkk.tagger.infrastructure.annotation.TaggerMapping
 import java.lang.reflect.ParameterizedType
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.jvm.jvmName
 
 @Component
 class TaggerFrontController {
     @Autowired
     private lateinit var context: ApplicationContext
-    private val controllers = HashMap<String, TaggerController<Any>>()
-    private val documentation = HashMap<String, HashMap<String, String>>()
+    private val controllers: MutableMap<String, TaggerController<Any>> = HashMap()
+    private val documentation: MutableMap<String, MutableMap<String, String>> = HashMap()
 
     companion object {
         private val log: Logger = LoggerFactory.getLogger(this::class.java)
@@ -33,69 +35,103 @@ class TaggerFrontController {
     @PostConstruct
     private fun init() {
         val beans = this.context.getBeansWithAnnotation(TaggerMapping::class.java)
-        for (bean in beans) {
-            val clazz: KClass<*> = bean.value::class
-            val annotation = clazz.findAnnotation<TaggerMapping>()
-            controllers[annotation!!.value] = bean.value as TaggerController<Any>
-
-            val subDocumentation = HashMap<String, String>()
-            documentation[annotation.value] = subDocumentation
-            val annotatedMethods =
-                controllers[annotation.value]!!::class.memberFunctions.filter { it.findAnnotation<TaggerDocumented>() != null }
-            for (method in annotatedMethods) {
-                val taggerDocumentedAnnotation = method.findAnnotation<TaggerDocumented>()
-
-                subDocumentation[method.findAnnotation<TaggerArgsMapping>()?.value ?: "MAIN"] =
-                    taggerDocumentedAnnotation!!.description +
-                            if (taggerDocumentedAnnotation.example.isNotBlank())
-                                "\nПример: ${taggerDocumentedAnnotation.example}"
-                            else ""
+        controllers.putAll(
+            beans.toList().associateNotNull { (_, bean) ->
+                val clazz: KClass<*> = bean::class
+                val annotation = clazz.findAnnotation<TaggerMapping>()
+                val key = annotation?.value.takeIf { !it.isNullOrBlank() && it !in controllers }
+                    ?: clazz.jvmName
+                key to (bean as TaggerController<Any>)
             }
-        }
+        )
 
+        println(controllers)
+
+        initDocumentation()
+        injectDocumentation()
+    }
+
+    fun initDocumentation() {
+        controllers.forEach { (key, controller) ->
+            controller::class.memberFunctions
+                .filter { it.findAnnotation<TaggerDocumented>() != null }
+                .forEach { method ->
+                    val taggerDocumentedAnnotation = method.findAnnotation<TaggerDocumented>()
+                    val subArgsKey = method.findAnnotation<TaggerArgsMapping>()?.value ?: "MAIN"
+                    val description =
+                        taggerDocumentedAnnotation!!.description +
+                                if (taggerDocumentedAnnotation.example.isNotBlank())
+                                    "\nПример: ${taggerDocumentedAnnotation.example}"
+                                else ""
+
+                    documentation.computeIfAbsent(key) { HashMap() }[subArgsKey] = description
+                }
+        }
+    }
+
+    private fun injectDocumentation() {
         val taggerHelp =
             controllers.filter { it.value::class.hasAnnotation<TaggerHelp>() }.values.firstOrNull() ?: return
         try {
             val documentationField = taggerHelp.javaClass.getDeclaredField("documentation")
             documentationField.isAccessible = true
-            documentationField.set(taggerHelp, documentation)
+            documentationField[taggerHelp] = documentation
         } catch (e: NoSuchFieldException) {
             log.error(e.message)
         }
     }
 
-    fun map(message: Message): MessageBuilder? {
-        val args = message.text.split(" ")
+    fun dispatch(message: Message): MessageBuilder? {
         val subArgs = argSplit(message.text)
 
-        for (controller in controllers) {
-            if (args.first() in controller.key) {
-                var messageBuilder = MessageBuilder.Builder(message.peerId)
-                val c = controller.value
-                val dto =
-                    ((c.javaClass.genericInterfaces[0] as ParameterizedType).actualTypeArguments[0] as Class<*>).getDeclaredConstructor()
-                        .newInstance()
-                for (subArgsKey in subArgs.keys) {
-                    val annotatedMethods =
-                        c::class.memberFunctions.filter { it.findAnnotation<TaggerArgsMapping>() != null }
-                    for (method in annotatedMethods) {
-                        val annotation = method.findAnnotation<TaggerArgsMapping>()
-                        if (annotation!!.value == subArgsKey)
-                            messageBuilder =
-                                method.call(
-                                    c,
-                                    message,
-                                    subArgs[subArgsKey],
-                                    messageBuilder,
-                                    dto
-                                ) as MessageBuilder.Builder
+        for ((key, controller) in controllers) {
+            if (controller.mapping(message.text, key)) {
+                val messageBuilder = MessageBuilder.Builder(message.peerId)
+                val dto = createDtoInstance(controller)
+
+                for ((subArgsKey, method) in findAnnotatedMethods(controller)) {
+                    subArgs[subArgsKey]?.let {
+                        handleMethodInvocation(
+                            method,
+                            controller,
+                            message,
+                            it,
+                            messageBuilder,
+                            dto
+                        )
                     }
                 }
-                return controller.value.handle(message, subArgs, messageBuilder, dto)
+
+                return controller.handle(message, subArgs, messageBuilder, dto)
             }
         }
+
         return null
     }
+
+    private fun createDtoInstance(controller: TaggerController<Any>): Any {
+        val genericType =
+            (controller.javaClass.genericInterfaces[0] as ParameterizedType).actualTypeArguments[0] as Class<*>
+        return genericType.getDeclaredConstructor().newInstance()
+    }
+
+    private fun findAnnotatedMethods(controller: TaggerController<Any>): List<Pair<String, KFunction<*>>> {
+        return controller::class.memberFunctions
+            .filter { it.findAnnotation<TaggerArgsMapping>() != null }
+            .map { it.findAnnotation<TaggerArgsMapping>()!!.value to it }
+    }
+
+    private fun handleMethodInvocation(
+        method: KFunction<*>,
+        controller: TaggerController<Any>,
+        message: Message,
+        subArgs: Array<String>,
+        messageBuilder: MessageBuilder.Builder,
+        dto: Any
+    ) {
+        method.call(controller, message, subArgs, messageBuilder, dto)
+    }
+
 
     fun argSplit(input: String): Map<String, Array<String>> {
         val pattern = "(-\\S+)(?:\\s(?!-\\S+)(.*?)(?=\\s-\\S+|\$))?".toRegex()
@@ -113,4 +149,13 @@ class TaggerFrontController {
         }
         return result
     }
+}
+
+private inline fun <K, V, R : Any> Iterable<Pair<K, V>>.associateNotNull(transform: (Pair<K, V>) -> Pair<K, R>?): Map<K, R> {
+    val destination = LinkedHashMap<K, R>()
+    for (element in this) {
+        val (key, value) = transform(element) ?: continue
+        destination[key] = value
+    }
+    return destination
 }
